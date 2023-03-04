189cc9938a9de0e090adf19e338a4e2c64396307
Pranjaliyadav <yadavpranjali1223@gmail.com>
2023-03-04 16:15:21 +0530

    d31

---
+++ 75DaysInterviews/698.java
@@ -0,0 +1,36 @@
+class Solution {
+   public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        int[] bucket = new int[k];
+        for(int i : nums){ sum +=i;}
+
+        if(sum %k != 0 || nums.length < k) return false;
+
+        Arrays.sort(nums);
+
+        return helper(nums,sum/k,nums.length-1,bucket);
+    }
+
+    public boolean helper(int[] nums, int target, int i, int[] bucket){
+
+        if(i == -1) return true;
+
+        for(int j = 0; j < bucket.length;j++){
+            if(bucket[j] + nums[i] <= target){
+                bucket[j] = bucket[j] + nums[i];
+
+                if(helper(nums,target,i-1,bucket)){
+                    return true;
+                }
+
+                bucket[j] = bucket[j] - nums[i];
+
+            }
+
+            if(bucket[j] == 0){
+                break;
+            }
+        }
+        return false;
+    }
+}

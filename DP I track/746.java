ca0fb38d9a4dc13ff028a995b0a51458246e11b9
Pranjaliyadav <yadavpranjali1223@gmail.com>
2023-03-12 14:23:52 +0530

    dp2

---
+++ DP I track/746.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int s1 = 0;
+        int s2 = 0;
+
+        for(int i = cost.length - 1; i >= 0 ; i--){
+            int curr = cost[i] + Math.min(s1,s2);
+            s1 = s2;
+            s2 = curr;
+        }
+        return Math.min(s1,s2);
+    }
+}

c950bd986300aae7a18e95c898447a8e1192ccc6
Pranjaliyadav <yadavpranjali1223@gmail.com>
2024-02-27 00:01:54 +0530

    c

--- Javascript/100-Sametrees.js
+++ Javascript/100-Sametrees.js
@@ -1,5 +1,5 @@
 var isSameTree = function(p, q) {
-    if(!p && !q) return true
-    if(!p || !q) return false
+    if(!p && !q) return true //checking if p and q both are null, return true, can be identical
+    if(!p || !q) return  //only one is null, not identical
     return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
